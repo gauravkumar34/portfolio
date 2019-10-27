02187b5ab353e53bb0e570e5fa7c426079985bf8
Gaurav Kumar <49734163+gauravkumar34@users.noreply.github.com>
2019-10-27 15:41:52 +0530

    Update instagram.js

--- js/instagram.js
+++ js/instagram.js
@@ -1,7 +1,7 @@
 var userFeed = new Instafeed({
   get: "user",
-  userId: "6205938688",
-  clientId: "2295525104",
+  userId: "2295525104",
+  clientId: "924f677fa3854436947ab4372ffa688d",
   accessToken: "f+9xGQXp4wlH+yScpXSIEHdgVJ4jxDEvkJTRsceuYZt+yc+5PSfTIJZYi0Dm+XtB",
   resolution: "standard_resolution",
   template: `<div class="col-md-3 "> <img src="{{image}}" class="instaPic"> </div>  `,
