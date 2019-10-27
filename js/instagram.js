68f4801cbbbe28ee3e7fc57589fb94464f9912a3
Gaurav Kumar <49734163+gauravkumar34@users.noreply.github.com>
2019-10-27 15:50:37 +0530

    insta api

--- js/instagram.js
+++ js/instagram.js
@@ -1,9 +1,11 @@
   
 var userFeed = new Instafeed({
   get: "user",
-  userId: "6205938688",
+  //userId: "6205938688",
+  userId: "2295525104",
   clientId: "924f677fa3854436947ab4372ffa688d",
-  accessToken: "6205938688.1677ed0.0cce5379b7d34511841a18952f731396",
+  //accessToken: "6205938688.1677ed0.0cce5379b7d34511841a18952f731396",
+  accessToken: "2295525104.d90570a.b250cde79e0a42ea9baf1a2f64361ac9",
   resolution: "standard_resolution",
   template: `<div class="col-md-3 "> <img src="{{image}}" class="instaPic"> </div>  `,
   sortBy: "most-recent",
